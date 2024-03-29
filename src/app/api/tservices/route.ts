bf7016e6bbc3dbda55c6922d71cf2c3c5cb572cc
gioocharkviani <gioocharkviani@gmail.com>
2024-03-29 10:55:58 +0400

    fix route error

--- src/app/api/tservices/route.ts
+++ src/app/api/tservices/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest , NextResponse } from "next/server";
 
-export async function  GET({req }: {req:NextRequest }) {
+export async function  GET(req:NextRequest) {
     return NextResponse.json('services')
 }
