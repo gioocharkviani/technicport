1520bf25ed724b781ddc714d43fd78039431793f
gioocharkviani <gioocharkviani@gmail.com>
2024-03-30 00:55:41 +0400

    adding some brands and adding swiper in brand component

--- src/components/brands/brandsComp.tsx
+++ src/components/brands/brandsComp.tsx
@@ -1,24 +1,76 @@
+'use client'
 import BrandsCard from "../cards/brandsCard";
-import Title3 from "../title/title3"
 import { useTranslations } from "next-intl";
-import motule from '../../../public/motul.png'
+
+import motule from '../../../public/brands/motul.png'
+import solas from '../../../public/brands/solas.png'
+import ngk from '../../../public/brands/ngk.png'
+import quicksilver from '../../../public/brands/quicksilver.png'
+import champione from '../../../public/brands/champion.png'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { FreeMode, Autoplay } from 'swiper/modules';
 
 
 const BrandsComp = () => {
   const t = useTranslations();
   return (
-    <div className="flex items-center overflow-hidden  gap-3 mt-[40px] bg-[#272a37] rounded-lg py-[20px] px-[20px]  ">
+    <div className="flex md:flex-row flex-col items-center overflow-hidden  gap-6 md:gap-3 mt-[40px] bg-[#272a37] rounded-lg py-[40px] px-[20px]  ">
 
-      <div className="text-[15] w-max shrink-0 text-white pr-[30px]">{t('partners.partnerTitle')}</div>
+      <div className="text-[18px] w-max shrink-0 text-white pr-[30px]">{t('partners.partnerTitle')}</div>
 
         <div className="flex gap-5 justify-between overflow-hidden w-full">
+
+        <Swiper
+        breakpoints={{
+          1000: {
+            slidesPerView: 5,
+          },
+          768: {
+            slidesPerView: 4,
+          },
+          500: {
+            slidesPerView: 3,
+          },
+          350: {
+            slidesPerView: 2,
+          },
+        }}
+        spaceBetween={50}
+        freeMode={true}
+        autoplay={{
+          delay: 2000, 
+          disableOnInteraction: false,
+        }}
+        modules={[FreeMode , Autoplay]}
+        className="swiper-brands"
+      >
+
+        <SwiperSlide>
             <BrandsCard  image={motule} />
-            <BrandsCard  image={motule} />
-            <BrandsCard  image={motule} />
-            <BrandsCard  image={motule} />
-            <BrandsCard  image={motule} />
-            <BrandsCard  image={motule} />
-            <BrandsCard  image={motule} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <BrandsCard  image={ngk} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <BrandsCard  image={solas} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <BrandsCard  image={champione} />
+        </SwiperSlide>
+
+        <SwiperSlide>
+            <BrandsCard  image={quicksilver} />
+        </SwiperSlide>
+        
+      </Swiper>
+
         </div>
 
     </div>
--- src/components/cards/brandsCard.tsx
+++ src/components/cards/brandsCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const BrandsCard = ({image}:{image:any}) => {
   return (
-    <div className="h-[100px]  rounded-md flex justify-center items-center">
+    <div className="h-[100px] hover:opacity-40 transition-all cursor-pointer w-auto rounded-md flex justify-center items-center">
       <Image src={image} alt='1' width={200} height={100} className='w-[200px] object-contain'/> 
     </div>
   )
