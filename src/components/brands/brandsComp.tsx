a9201c00f1ec4227d7167e4e5574693c1307eb75
gioocharkviani <gioocharkviani@gmail.com>
2024-03-29 10:48:15 +0400

    adasdw

--- src/components/brands/brandsComp.tsx
+++ src/components/brands/brandsComp.tsx
@@ -7,11 +7,17 @@ import motule from '../../../public/motul.png'
 const BrandsComp = () => {
   const t = useTranslations();
   return (
-    <div className="flex items-center gap-3 mt-[30px] bg-[#272a37] rounded-lg py-[20px] px-[20px]  ">
+    <div className="flex items-center overflow-hidden  gap-3 mt-[30px] bg-[#272a37] rounded-lg py-[20px] px-[20px]  ">
 
-      <div className="w-max text-[15] text-white pr-[30px]">{t('partners.partnerTitle')}</div>
+      <div className="text-[15] w-max shrink-0 text-white pr-[30px]">{t('partners.partnerTitle')}</div>
 
-        <div className="grid grid-cols-1 ssm:grid-cols-2 md:grid-cols-4 gap-3 justify-between">
+        <div className="flex gap-5 justify-between overflow-hidden w-full">
+            <BrandsCard  image={motule} />
+            <BrandsCard  image={motule} />
+            <BrandsCard  image={motule} />
+            <BrandsCard  image={motule} />
+            <BrandsCard  image={motule} />
+            <BrandsCard  image={motule} />
             <BrandsCard  image={motule} />
         </div>
 
--- src/components/contentwrapper/maxWidthWrapper.tsx
+++ src/components/contentwrapper/maxWidthWrapper.tsx
@@ -5,7 +5,7 @@ interface pageProps {
 
 const MaxWidthWrapper = ({children} : pageProps) => {
   return (
-    <div className="max-w-[1500px] w-full px-[10px] md:px-[30px]">
+    <div className="max-w-[1500px] overflow-hidden w-full px-[10px] md:px-[30px]">
         {children}
     </div>
   )
