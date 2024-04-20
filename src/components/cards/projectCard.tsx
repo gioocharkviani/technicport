fe1f7f0bc86131e424d7759decb800dc3dff6b14
gioocharkviani <gioocharkviani@gmail.com>
2024-04-20 13:29:47 +0400

    adding some style for project

--- src/components/cards/projectCard.tsx
+++ src/components/cards/projectCard.tsx
@@ -4,7 +4,7 @@ import accet2 from '../../../public/accet2.png'
 
 const ProjectCard = () => {
   return (
-    <div className=' gridprjItem cursor-pointer hover:opacity-50 transition-all relative flex items-center justify-center overflow-hidden'>
+    <div className='gridprjItem cursor-pointer relative flex items-center justify-center overflow-hidden'>
         <div className='absolute bottom-[10%] bg-[#e4e4e4] rounded-r-sm py-[6px] px-[20px] text-[15px] capitalize left-0 w-max h-max'>
           Project some title
         </div>
