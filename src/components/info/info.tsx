a4773e7de9bff7953aca84839c055229111a15f8
gioocharkviani <gioocharkviani@gmail.com>
2024-05-01 12:42:49 +0400

    fix fetching problems

--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -6,6 +6,7 @@ import axios from 'axios';
 import { useLocale } from '@/hooks/locale-provider';
 
 const Info = () => {
+
   const [data, setData] = useState<any>(null);
   const locale = useLocale();
 
@@ -13,9 +14,9 @@ const Info = () => {
     const fetchData = async () => {
       try {
         const response = await axios.get('/api/about');
-        setData(response.data); // Store the response data in the state
+        setData(response.data); 
       } catch (error) {
-        console.log(error);
+        return null
       }
     };
 
@@ -28,7 +29,7 @@ const Info = () => {
     <div className='w-full flex h-full flex-col bg-[#FFF] rounded-lg py-[20px] px-[20px] '>
       <Title1 title={t('info.header')} moreInfo={t('global.moreInfo')} link={`/about`} />
       <ul className='infoUl px-[20px] md:px-[0] h-[200px] relative overflow-y-auto flex flex-col gap-3 mt-[20px] text-[13px]'>
-        <li>{data ? data : null}</li> 
+        <li>{data ? data : 'null'}</li> 
       </ul>
     </div>
   );
